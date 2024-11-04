e4b69e80c1067b61b001a6b2e604560ce27c11a3
Edwin Cardenas <eacardenase@gmail.com>
2024-11-03 19:38:06 -0500

    feat: add project structure

--- bank.go
+++ bank.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Bank App")
+	fmt.Print("Welcome to the Bank Application\n\n")
+
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check the balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Your choice:", choice)
 }
