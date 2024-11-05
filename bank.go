0658330ed10b76c92f3209dbf4976ab35be17570
Edwin Cardenas <eacardenase@gmail.com>
2024-11-05 09:15:55 -0500

    refactor: split code into multiple files

--- bank.go
+++ bank.go
@@ -9,14 +9,6 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func displayUserOptions() {
-	fmt.Println("\nWhat do you want to do?")
-	fmt.Println("1. Check the balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Print("4. Exit\n\n")
-}
-
 func getUserInput() int {
 	var choice int
 	fmt.Print("Select your choice: ")
---
+++ communication.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func displayUserOptions() {
+	fmt.Println("\nWhat do you want to do?")
+	fmt.Println("1. Check the balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Print("4. Exit\n\n")
+}
